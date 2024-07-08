f54c5952afc9783dc8095b578c86f8f30c019a87
humandotlearning <nithin.varghese1357@gmail.com>
2024-07-09 01:26:04 +0530

    + edit option for blogs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/humandotlearning/humandotlearning.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/humandotlearning/humandotlearning.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -103,8 +103,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'About Me',
+                to: '/aboutMe',
               },
               {
                 label: 'Blog',
